49bfa0ee1998ad3af590e12936922f34d72a94b7
Payom Meshgin <meshginpayom@gmail.com>
2013-11-27 10:16:00 -0500

    Create cholOMP.c

---
+++ cholOMP.c
@@ -0,0 +1,27 @@
+double ** chol_omp(double ** A, int n){
+	// Copy matrix A and take only lower triangular part
+	double ** L = init_matrix(n);
+	trans_copy(A, L, n);
+	
+	time(&start);
+	
+	int i,j,k;
+	#pragma omp parallel for shared(L) private(j)
+	for (j = 0; j < n; j++){
+		
+		for (k = 0; k < j; k++){
+			// Inner sum
+			for (i = j; i < n; i++){
+				L[i][j] = L[i][j] - L[i][k] * L[j][k];
+			}
+		}
+		
+		L[j][j] = sqrt(L[j][j]);
+		
+		for (i = j+1; i < n; i++){
+			L[i][j] = L[i][j]/ L[j][j];
+		}
+	}
+	
+	return L;
+}
